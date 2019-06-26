9f2cae6599f997602cc38b05b09db28b33c85329
D. V. Wiebe <dvw@phas.ubc.ca>
2019-06-26 14:59:58 -0700

    Import all the table classes into the base package for simplicity.
    Then we can all do:
    
            import chimedb.data_index as di
    
    and feel good about ourselves.

--- chimedb/data_index/__init__.py
+++ chimedb/data_index/__init__.py
@@ -1,2 +1,10 @@
 """alpenhorn (data index) table definitions"""
+from .orm import (
+        AcqType, ArchiveAcq, ArchiveFile, ArchiveFileCopy,
+        ArchiveFileCopyRequest, ArchiveInst, CalibrationGainFileInfo,
+        CorrAcqInfo, CorrFileInfo, DigitalGainFileInfo, FileType,
+        FlagInputFileInfo, HKAcqInfo, HKFileInfo, HKPFileInfo, RawadcAcqInfo,
+        RawadcFileInfo, StorageGroup, StorageNode,
+)
+
 __version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-from chimedb.data_index import __version__
+import codecs
+import os
+import re
+
+# Get the version from __init__.py without having to import it.
+def _get_version():
+    with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+        "chimedb", "data_index", "__init__.py"), 'r') as init_py:
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                init_py.read(), re.M)
+
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name='chimedb.data_index',
-    version=__version__,
+    version=_get_version(),
 
     packages=['chimedb.data_index'],
     zip_safe=False,
