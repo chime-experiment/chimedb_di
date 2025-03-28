d79cbb2f475504fb309150a2d72f95a81a6d3eeb
D. V. Wiebe <dvw@phas.ubc.ca>
2025-03-28 10:55:53 -0700

    refactor(util): Delete alpenhorn-1 import routines
    This deletes all the routines used by alpenhorn-1 for file
    detection and registration from the `util` module.  This
    functionality was all re-implemented for alpenhorn-2 in #21
    and https://github.com/chime-experiment/alpenhorn-chime/pull/1
    
    All the file regexes now live in the `FileType` table in the database.
    
    This could all just stay here and languish as unused dead code
    without interfering, I suppose, but I think it's better to delete
    it all to prevent future code devs by being misled by its presence.
    
    I really don't understand why this code ended up here instead of
    just being part of alpenhorn-1.  Perhaps it was simply missed when
    alpenhorn was first split off from `ch_util` and then moved here when
    this repository was similarly blebbed off.  Or perhaps there were plans
    for some sort of non-alpenhorn-y way of adding files to the data index
    that never came to fruition.  Whatever the reasoning, in reality, it's
    never been referenced by anything other than alpenhorn-1.
    
    **Note**: We are still running an alpenhorn-1 instance on niagara to
    manage our HPSS archive.  However we shouldn't be importing new files
    there, so while that instance _could_ call into this code, in practice
    it never will.
    
    I'd say if we need to redeploy the instance on niagara (and we don't
    want to upgrade to alpenhorn-2 at that time), then a better solution
    would be to disable the import code in that deployment's instance
    instead of keeping this code around here so it could be referenced but
    never used.

--- chimedb/data_index/util.py
+++ chimedb/data_index/util.py
@@ -1,9 +1,7 @@
 """
-Helper routines for parsing CHIME data archive records
+Helper routines for working with the CHIME data index
 
 .. currentmodule:: chimedb.data_index.util
-
-This module contains utility routines for parsing CHIME data archive metadata
 
 
 Routines
@@ -12,18 +10,10 @@ Routines
 .. autosummary::
     :toctree: generated/
 
-    detect_file_type
-    md5sum_file
-    parse_acq_name
-    parse_corrfile_name
-    parse_hfbfile_name
-    parse_hkfile_name
-    parse_weatherfile_name
-    populate_types
+    update_types
+    update_inst
+    update_storage
 """
-
-import os
-import re
 
 import chimedb.core as db
 
@@ -36,227 +26,6 @@ from .orm import (
     StorageGroup,
     StorageTransferAction,
 )
-
-# Global variables
-# ================
-
-fname_atmel = "atmel_id.dat"
-
-_fmt_acq = re.compile(r"([0-9]{8})T([0-9]{6})Z_([A-Za-z0-9]*)_([A-Za-z]*)")
-_fmt_corr = re.compile(r"([0-9]{8})_([0-9]{4})\.h5")
-_fmt_hfb = re.compile(r"hfb_([0-9]{8})_([0-9]{4})\.h5")
-_fmt_hk = re.compile(r"([A-Za-z]*)_([0-9]{8})\.h5")
-_fmt_hkp = re.compile(r"hkp_prom_([0-9]{8})\.h5")
-_fmt_atmel = re.compile(r"atmel_id\.dat")
-_fmt_log = re.compile(r"ch_(master|hk)\.log")
-_fmt_rawadc = re.compile(r"rawadc\.npy")
-_fmt_rawadc_hist = re.compile(r"histogram_chan([0-9]{1,2})\.pdf")
-_fmt_rawadc_spec = re.compile(r"spectrum_chan([0-9]{1,2})\.pdf")
-_fmt_rawadc_h5 = re.compile(r"[0-9]{6}\.h5")
-_fmt_raw_gains = re.compile(r"(gains|gains_noisy)\.pkl")
-_fmt_weather = re.compile(r"(20[12][0-9][01][0-9][0123][0-9])\.h5")
-_fmt_calib_data = re.compile(r"\d{8}\.h5")
-_fmt_misc_tar = re.compile(
-    r"([0-9]{8})_([A-Za-z][A-Za-z0-9_+-]*)\.misc\.tar(?:\.gz|\.bz2|\.xz)"
-)
-
-
-# Helper routines for adding files
-# ================================
-
-
-def md5sum_file(filename, hr=True, cmd_line=False):
-    """Find the md5sum of a given file.
-
-    Output should reproduce that of UNIX md5sum command.
-
-    Parameters
-    ----------
-    filename: string
-        Name of file to checksum.
-    hr: boolean, optional
-        Should output be a human readable hexstring (default is True).
-    cmd_line: boolean, optional
-        If True, then simply do an os call to md5sum (default is False).
-
-    See Also
-    --------
-    http://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-
-    python
-    """
-    if cmd_line:
-        p = os.popen("md5sum %s 2> /dev/null" % filename, "r")
-        res = p.read()
-        p.close()
-        md5 = res.split()[0]
-        assert len(md5) == 32
-        return md5
-    else:
-        import hashlib
-
-        block_size = 256 * 128
-
-        md5 = hashlib.md5()
-        with open(filename, "rb") as f:
-            for chunk in iter(lambda: f.read(block_size), b""):
-                md5.update(chunk)
-        if hr:
-            return md5.hexdigest()
-        return md5.digest()
-
-
-def parse_acq_name(name):
-    """Validate and parse an acquisition name.
-
-    Parameters
-    ----------
-    name : The name of the acquisition.
-
-    Returns
-    -------
-    A tuple of timestamp, instrument and type.
-
-    """
-    if not re.match(_fmt_acq, name):
-        raise db.ValidationError('Bad acquisition name format for "%s".' % name)
-    ret = tuple(name.split("_"))
-    if len(ret) != 3:
-        raise db.ValidationError('Bad acquisition name format for "%s".' % name)
-    return ret
-
-
-def parse_corrfile_name(name):
-    """Validate and parse a correlation file name.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    A tuple of seconds from start and freqency index.
-    """
-    m = re.match(_fmt_corr, name)
-    if not m:
-        raise db.ValidationError('Bad correlator file name format for "%s".' % name)
-
-    return int(m.group(1)), int(m.group(2))
-
-
-def parse_hfbfile_name(name):
-    """Validate and parse a HFB file name.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    A tuple of seconds from start and freqency index.
-    """
-    m = re.match(_fmt_hfb, name)
-    if not m:
-        raise db.ValidationError('Bad HFB file name format for "%s".' % name)
-
-    return int(m.group(1)), int(m.group(2))
-
-
-def parse_weatherfile_name(name):
-    """Validate and parse a weather file name.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    The date string, of format YYYYMMDD.
-    """
-    m = re.match(_fmt_weather, name)
-    if not m:
-        raise db.ValidationError('Bad weather file name format for "%s".' % name)
-
-    return name[0:8]
-
-
-def parse_miscfile_name(name):
-    """Validate and parse a misc file name.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    A tuple of containing the eight-digit serial number and the misc data
-    type.
-    """
-    m = re.match(_fmt_misc_tar, name)
-    if not m:
-        raise db.ValidationError(
-            'Bad miscellaneous file name format for "{0}".'.format(name)
-        )
-    return int(m.group(1)), m.group(2)
-
-
-def parse_hkfile_name(name):
-    """Validate and parse a correlation file name.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    A tuple of seconds from start and the ATMEL name.
-    """
-    m = re.match(_fmt_hk, name)
-    if not m:
-        raise db.ValidationError('Bad correlator file name format for "%s".' % name)
-
-    return int(m.group(2)), m.group(1)
-
-
-def detect_file_type(name):
-    """Figure out what kind of file this is.
-
-    Parameters
-    ----------
-    name : The name of the file.
-
-    Returns
-    -------
-    An object of FileType, or None if unrecognised.
-
-    """
-
-    if re.match(_fmt_corr, name):
-        return FileType.get(name="corr")
-    elif re.match(_fmt_hfb, name):
-        return FileType.get(name="hfb")
-    elif re.match(_fmt_hk, name):
-        return FileType.get(name="hk")
-    elif re.match(_fmt_hkp, name):
-        return FileType.get(name="hkp")
-    elif re.match(_fmt_log, name):
-        return FileType.get(name="log")
-    elif re.match(_fmt_atmel, name):
-        return FileType.get(name="atmel_id")
-    elif re.match(_fmt_rawadc, name) or re.match(_fmt_rawadc_h5, name):
-        return FileType.get(name="rawadc")
-    elif re.match(_fmt_rawadc_hist, name) or re.match(_fmt_rawadc_spec, name):
-        return FileType.get(name="pdf")
-    elif re.match(_fmt_raw_gains, name):
-        return FileType.get(name="pkl")
-    elif re.match(_fmt_weather, name):
-        return FileType.get(name="weather")
-    elif re.match(_fmt_calib_data, name):
-        return FileType.get(name="calibration")
-    elif re.match(_fmt_misc_tar, name):
-        return FileType.get(name="miscellaneous")
-    else:
-        return None
-
 
 # Routines for setting up the database
 # ====================================
@@ -782,7 +551,14 @@ def update_storage():
         }
 
     nodes = [
-        _node(11, "gong", "drao_storage", auto_import=True, storage_type="A"),
+        _node(
+            11,
+            "gong",
+            "drao_storage",
+            io_class="Polling",
+            auto_import=True,
+            storage_type="A",
+        ),
         _node(1117, "cedar_offload", "cedar_offload", storage_type="A"),
         _node(
             1119,
